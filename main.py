9f80afe370ebd6ea726bb40b05e29b7be80a4d41
a1685823 <a1685823@student.adelaide.edu.au>
2019-12-12 19:38:01 +1030

    Serial settings UI written.

--- main.py
+++ main.py
@@ -14,11 +14,38 @@
 
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtWidgets import QPushButton, QMessageBox, QGridLayout
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 import serial
+
+class GroupBox(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.setWindowTitle("GroupBox")
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        groupbox = QGroupBox("GroupBox Example")
+        groupbox.setCheckable(True)
+        layout.addWidget(groupbox)
+
+        vbox = QVBoxLayout()
+        groupbox.setLayout(vbox)
+
+        radiobutton = QRadioButton("RadioButton 1")
+        vbox.addWidget(radiobutton)
+
+        radiobutton = QRadioButton("RadioButton 2")
+        vbox.addWidget(radiobutton)
+
+        radiobutton = QRadioButton("RadioButton 3")
+        vbox.addWidget(radiobutton)
+
+        radiobutton = QRadioButton("RadioButton 4")
+        vbox.addWidget(radiobutton)
 
 def buttonStartBehaviour():
 
@@ -32,37 +59,93 @@ def buttonStartBehaviour():
     if returnValue == QMessageBox.Ok:
         print('Ok clicked')
 
+def buttonSaveBehaviour():
+
+    msgBox = QMessageBox()
+    msgBox.setWindowTitle("Start Connection")
+    msgBox.setIcon(QMessageBox.Information)
+    msgBox.setText("Connection has been started.")
+    msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
 def window():
 
     # qt init
     app = QApplication(sys.argv)
     widget = QWidget()
-
-    # labels show text or images
-    textLabel = QLabel(widget)
-    textLabel.setText("Sensarray")
-    textLabel.move(110, 85)
-
-    # start button
-    buttonStartConnect = QPushButton(widget)
-    buttonStartConnect.setText('Start')
-    buttonStartConnect.move(64, 32)
-    buttonStartConnect.clicked.connect(buttonStartBehaviour)
-
-    # stop button
-    buttonStopConnect = QPushButton(widget)
-    buttonStopConnect.setText('Stop')
-    buttonStopConnect.move(64, 64)
-
-    # sensor grid
-    grid = QGridLayout()
-
-
-    # start position of window, and size
-    widget.setGeometry(50, 50, 320, 200)
+    widget.setGeometry(50, 50, 320, 320)
     widget.setWindowTitle("Sensarray")
-    widget.show()
+    vbLayout = QVBoxLayout()
+    widget.setLayout(vbLayout)
 
+    ### serial settings groupbox
+    gbSerial = QGroupBox("Serial Settings")
+    glSerial = QGridLayout()
+    gbSerial.setLayout(glSerial)
+
+    # port name
+    lPortName = QLabel()
+    lPortName.setText("Port Name:")
+    glSerial.addWidget(lPortName, 0, 0)
+
+    # port combo
+    cbPorts = QComboBox()
+    cbPorts.addItem('COM1')
+    cbPorts.addItem('COM2')
+    cbPorts.addItem('COM3')
+    glSerial.addWidget(cbPorts, 0, 1)
+
+    # baud rate
+    lBaudRate = QLabel()
+    lBaudRate.setText("Baud Rate:")
+    glSerial.addWidget(lBaudRate, 1, 0)
+
+    # baud combo
+    cbBaudRate = QComboBox()
+    cbBaudRate.addItem('9600')
+    cbBaudRate.addItem('19200')
+    cbBaudRate.addItem('38400')
+    cbBaudRate.addItem('57600')
+    cbBaudRate.addItem('115200')
+    glSerial.addWidget(cbBaudRate, 1, 1)
+
+    # data bits
+    lDataBits = QLabel()
+    lDataBits.setText('Data Bits:')
+    glSerial.addWidget(lDataBits, 2, 0)
+
+    # data bits QComboBox
+    cbDataBits = QComboBox()
+    cbDataBits.addItem('8')
+    cbDataBits.addItem('7')
+    glSerial.addWidget(cbDataBits, 2, 1)
+
+    # parity
+    lParity = QLabel()
+    lParity.setText('Parity:')
+    glSerial.addWidget(lParity, 3, 0)
+
+    # parity combo
+    cbParity = QComboBox()
+    cbParity.addItem('None')
+    cbParity.addItem('Even')
+    cbParity.addItem('Odd')
+    glSerial.addWidget(cbParity, 3, 1)
+
+    # stop bits
+    lStopBits = QLabel()
+    lStopBits.setText('Stop Bits:')
+    glSerial.addWidget(lStopBits, 4, 0)
+
+    # stop bits combo
+    cbStopBits = QComboBox()
+    cbStopBits.addItem('One')
+    cbStopBits.addItem('Two')
+    cbStopBits.addItem('None')
+    glSerial.addWidget(cbStopBits, 4, 1)
+
+    # add items
+    vbLayout.addWidget(gbSerial)
+    widget.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
